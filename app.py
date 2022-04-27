9dcdfce8f7457653a8b5ee63bf54ad58d416ea39
Rafael <perezaguirrerafael@gmail.com>
2022-04-27 18:53:34 -0500

    form login

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, redirect, render_template, request
+from flask import Flask, redirect, render_template, request, flash, url_for
 import requests
 import firebase_admin
 from firebase_admin import credentials, firestore
 import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '65gf4h65gf4h6gfh46fg4h65gfh65gfh465f'
 # Configuraciones para firebase
 cred = credentials.Certificate('serviceAccountKey.json')
 fire = firebase_admin.initialize_app(cred)
@@ -12,7 +13,19 @@ db = firestore.client()
 tasks_ref = db.collection('tasks')
 users_ref = db.collection('users')
 API_KEY = 'AIzaSyDpWOwvHcvnL3aamIZpUwvORgFaST8lI_4'
+user_auth = False
 
+def login_firebase(email, password):
+    credentials = {"email":email,"password":password,"returnSecureToken":True}
+    response = requests.post('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={}'.format(API_KEY),data=credentials)
+    if response.status_code == 200:
+        content = response.content
+        data = response.json()
+        print(data['localId'])
+    elif response.status_code == 400:
+        print(response.content)
+
+    return response.content
 
 def read_tasks(ref):
     docs = ref.get()
@@ -35,28 +48,55 @@ def update_task(ref,id):
 def delete_task(ref, id):
     res = ref.document(id).delete()
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global user_auth
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:#POST
+        email = request.form["email"]
+        password = request.form["password"]
+        print(f'{email}:{password}')
+        try:
+            if email == 'linares@correo.com' and password == 'pass123':
+                print("Iniciaste sesión...")
+                user_auth = True
+                return redirect('/')
+            else:
+                print("Sesion fallida...")
+                flash('Credenciales invalidas')
+                return redirect('/')
+        except:
+            print("Sesion fallida...")
+            flash('Credenciales invalidas')
+            return redirect('/')
+
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     if request.method == 'GET':
-        try:
-            tasks = read_tasks(tasks_ref)
-            completed = []
-            incompleted = []
-            for task in tasks:
-                print(task['check'])
-                if task['check'] == True:
-                    completed.append(task)
-                else:
-                    incompleted.append(task)
-        except:
-            print("Error...")
-            tasks = []
+        if user_auth:
+            try:
+                tasks = read_tasks(tasks_ref)
+                completed = []
+                incompleted = []
+                for task in tasks:
+                    print(task['check'])
+                    if task['check'] == True:
+                        completed.append(task)
+                    else:
+                        incompleted.append(task)
+            except:
+                print("Error...")
+                tasks = []
 
-        response = {'completed':completed, 
-                    'incompleted':incompleted,
-                    'counter1':len(completed),
-                    'counter2':len(incompleted)}
-        return render_template('index.html', response=response)
+            response = {'completed':completed, 
+                        'incompleted':incompleted,
+                        'counter1':len(completed),
+                        'counter2':len(incompleted)}
+            return render_template('index.html', response=response)
+        else:
+            return redirect(url_for('login'))
     else: #POST
         name = request.form["name"]
         print(f"\n{name}\n")
